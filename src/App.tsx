af50e69b2bf40334b19cd9a984c04df648a7ddee
OneBigUnit <08klock@brightoncollege.net>
2022-11-02 11:46:49 +0000

    templated structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import './App.css';
+import { Center, Container, Heading, HStack, VStack } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div/>
+    <VStack h={"100%"}>
+      <Center>
+        
+      </Center>
+      <HStack spacing={0} alignItems={"end"} flexGrow={1} maxW={"98%"}>
+
+      </HStack>
+      <Container h={"14%"} maxW={"80vw"} p={"24px"}>
+        <HStack spacing={"10%"} justifyContent={"center"}>
+          <VStack w={"20%"}>
+            
+          </VStack>
+          <VStack w={"10%"}>
+          </VStack>
+        </HStack>
+      </Container>
+    </VStack>
   );
 }
 
