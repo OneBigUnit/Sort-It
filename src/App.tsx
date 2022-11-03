65e36c70a8da7e23ef28aa28a9655ff35e94ae91
OneBigUnit <08klock@brightoncollege.net>
2022-11-03 08:41:55 +0000

    add sort creation mechanisms

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { useState } from 'react';
 import './App.css';
-import { Center, Container, Heading, HStack, VStack } from '@chakra-ui/react'
+import { Center, Container, Heading, HStack, VStack } from '@chakra-ui/react';
+import SortingAlgorithm from './sortingAlgorithm';
+import QuickSort from './sortingAlgorithms/quickSort';
+import BubbleSort from './sortingAlgorithms/bubbleSort';
 
-function App() {
+const INITIAL_ITEMS_NUMBER = 100
+
+const App = () => {
+  const randomArray = (length: number) => (
+    Array(length).fill(0).map(() => Math.round(Math.random() * length))
+  )
+  const shuffledRangeArray = (length: number) => (
+    [...Array(length).keys()].map(it => [it, Math.random()]).sort((a, b) => a[1] - b[1]).map(it => it[0] + 1)
+  )
+  const inverseSortedArray = (length: number) => (
+    [...Array(length).keys()].map(it => it + 1).reverse()
+  )
+
+  const sorts: {[name: string]: any} = {
+    "Bubble Sort": BubbleSort,
+    "Quick Sort": QuickSort
+  }
+  const creationTypes: {[name: string]: (length: number) => number[]} = {
+    "Random": randomArray,
+    "Range": shuffledRangeArray, 
+    "Inverse": inverseSortedArray
+  }
+
+  const [itemsNumber, setItemsNumber] = useState(INITIAL_ITEMS_NUMBER)
+  const [items, setItems] = useState<number[]>(shuffledRangeArray(itemsNumber))
+  const [activeSort, setSort] = useState<SortingAlgorithm | null>(null)
+
   return (
     <VStack h={"100%"}>
       <Center>
@@ -22,5 +52,7 @@ function App() {
     </VStack>
   );
 }
+
+export default App
 
 export default App;
